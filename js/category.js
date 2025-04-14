a0761112fe800e6c652633a2b48776db0aa2cb73
JoM1njun <165036607+JoM1njun@users.noreply.github.com>
2025-04-14 15:43:38 +0900

    Create category.js

---
+++ js/category.js
@@ -0,0 +1,115 @@
+function searchPlaces(category) {
+  var markersrc = "/public/marker/marker.svg";
+  var redmarkersrc = "/public/marker/red-marker.svg";
+  var imageSize = new kakao.maps.Size(75, 30); // 마커이미지의 크기
+
+  var markerImage = new kakao.maps.MarkerImage(markersrc, imageSize);
+  var redmarkerImage = new kakao.maps.MarkerImage(redmarkersrc, imageSize);
+  var activemarker = null;
+
+  if (category) {
+    fetch(
+      `http://localhost:3000/api/db-status?query=` + encodeURIComponent(category)
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Received : ", data);
+        const places = data.places; // places로 수정
+        console.log("Places : ", places);
+
+        // 기존 마커와 infoWindow 제거
+        userMarker.forEach((obj) => {
+          if (obj.infoWindow) {
+            obj.infoWindow.close();
+          }
+          if (obj.marker) {
+            obj.marker.setMap(null);
+          }
+        });
+        userMarker = [];
+
+        // 기존 infoWindows도 초기화
+        infoWindows.forEach((iw) => iw.close());
+        infoWindows = [];
+
+        if (window.innerWidth <= 768) {
+          map.setLevel(4);
+        } else {
+          map.setLevel(3);
+        }
+
+        if (places && places.length > 0) {
+          places.forEach((place, index) => {
+            console.log("위치 : ", place.latitude, place.longitude);
+            let placeLocation = new kakao.maps.LatLng(
+              place.latitude,
+              place.longitude
+            );
+
+            let Marker = new kakao.maps.Marker({
+              position: placeLocation,
+              map: map,
+              image: markerImage,
+              clickable: true,
+            });
+
+            let content = `
+                    <div class="info-window">
+                        <h4>${place.name}</h4>
+                        <p>${place.etc}</p>
+                    </div>`;
+
+            let infoWindow = new kakao.maps.InfoWindow({
+              content: content,
+              zIndex: 1,
+            });
+
+            Marker.infoWindow = infoWindow;
+            infoWindows.push(infoWindow);
+
+            userMarker.push({
+                marker: Marker,
+                infoWindow: infoWindow,
+              });
+
+            // 마커 클릭 시 기존 열린 창 닫고 해당 창 열기
+            kakao.maps.event.addListener(Marker, "click", function () {
+              // 모든 마커의 infoWindow 닫기
+              infoWindows.forEach((iw) => iw.close());
+
+              // 클릭한 마커의 infoWindow 열기
+              infoWindow.open(map, Marker);
+
+              if (activemarker) {
+                activemarker.setImage(markerImage);
+              }
+              Marker.setImage(redmarkerImage);
+              activemarker = Marker;
+
+              if (window.innerWidth <= 768) {
+                map.setLevel(3);
+              } else {
+                map.setLevel(2);
+              }
+              map.panTo(Marker.getPosition());
+            });
+          });
+          // 지도 클릭 시 모든 마커를 파란색으로 변경
+          kakao.maps.event.addListener(map, "click", function () {
+            if (activemarker) {
+              activemarker.setImage(markerImage);
+              activemarker = null;
+            }
+            infoWindows.forEach((iw) => iw.close());
+          });
+        } else {
+          alert(`[${category}] 카테고리에서 찾을 수 있는 장소가 없습니다.`);
+        }
+      })
+      .catch((err) => {
+        console.error(`[${category}] API 호출 오류`, err);
+      });
+  } else {
+    alert("카테고리를 선택하세요.");
+  }
+}
