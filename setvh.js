d94a87205e58ee09006ea32993d8da4565f68aee
JoM1njun <165036607+JoM1njun@users.noreply.github.com>
2025-04-15 18:37:22 +0900

    Delete setvh.js

--- setvh.js
+++
@@ -1,6 +0,0 @@
-function setVhUnit() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-setVhUnit();
-window.addEventListener('resize', setVhUnit);
