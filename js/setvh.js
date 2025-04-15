5d730f33e2b41f3b173fd35d472280c03ee72a42
JoM1njun <165036607+JoM1njun@users.noreply.github.com>
2025-04-15 19:07:12 +0900

    Update setvh.js

--- js/setvh.js
+++ js/setvh.js
@@ -2,5 +2,9 @@ function setVhUnit() {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setPropert('--vh', `${vh}px`);
 }
-setVhUnit();
-window.addEventListener('resize', setVhUnit);
+
+// DOM이 준비된 후 실행되도록!
+window.addEventListener('DOMContentLoaded', () => {
+  setVhUnit();
+  window.addEventListener('resize', setVhUnit);
+});
