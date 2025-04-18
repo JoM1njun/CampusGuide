cef22aed0597f4f40e445027362ad4a378b7decc
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-04-18 14:37:06 +0900

    Update place-hidden.js

--- js/place-hidden.js
+++ js/place-hidden.js
@@ -18,20 +18,41 @@ document.addEventListener("DOMContentLoaded", function () {
         placeMenu.style.display = "none";
       }
   });
+  
   // 지도 클릭 시 장소 목록을 숨기기
-  if (window.map) {
+  if (map) {
     kakao.maps.event.addListener(map, "click", function () {
       console.log("Map Click");
       // 장소 목록과 정보창을 숨깁니다.
-      placeMenu.style.display = "none";
-      placeinfo.style.display = "none";
+      placeMenu.style.display = "none"; // 장소 목록 숨기기
+      placeinfo.style.display = "none"; // 정보창 숨기기
 
       // 정보창 상태 업데이트
       isInfoVisible = false;
-      showPlaceButtons();
+      showPlaceButtons(); // 장소 목록을 다시 보이게
       console.log("Hidden");
     });
   } else {
     console.error("Map is not initialized yet.");
   }
 });
+
+// 장소 정보창을 닫을 때 호출되는 함수
+function closeInfo() {
+  const placeInfo = document.getElementById("place-info");
+  placeInfo.style.display = "none"; // 창을 숨깁니다.
+  isInfoVisible = false; // 정보창이 닫혔으므로 상태를 false로 설정
+  showPlaceButtons();
+}
+
+// 메뉴 버튼 클릭 시 장소 목록을 표시하고 정보창 상태 확인
+function showPlaceButtons() {
+  const placeButtons = document.getElementById("place-buttons");
+  if (!isInfoVisible) {
+    // 정보창이 보이지 않으면 장소 목록을 표시
+    placeButtons.style.display = "flex";
+  } else {
+    // 정보창이 열려 있다면 장소 목록을 숨깁니다.
+    placeButtons.style.display = "none";
+  }
+}
