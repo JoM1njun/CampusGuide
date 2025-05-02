ae049ca341509114f119e99417d114689a983681
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-05-03 02:54:49 +0900

    Update place-hidden.js

--- js/place-hidden.js
+++ js/place-hidden.js
@@ -20,7 +20,6 @@ function setupMapClickEvent() {
 
   kakao.maps.event.addListener(map, "click", function () {
     console.log("✅ 지도 클릭됨!");
-
     closeMenu();
   });
 }
@@ -33,11 +32,7 @@ function setupMapClickEvent() {
     toggleBtn.innerHTML = isOpen ? "&gt;" : "&lt;";
     
     // 버튼 위치 이동 (메뉴가 열리면 왼쪽으로 밀기)
-    toggleBtn.style.right = isOpen ? "26.5vw" : "0";
-
-    if (isOpen) {
-      placeInfo.style.display = "none"; // 메뉴 열릴 때 정보창 닫기
-    }
+    toggleBtn.style.right = isOpen ? "27vw" : "0";
   });
 
 // 문서 클릭 시 외부 클릭 감지 → 메뉴/정보창 닫기
