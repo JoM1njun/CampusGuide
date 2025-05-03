d2113d8db769e903c0bb27e66d2741b6052e0062
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-05-03 13:53:30 +0900

    Update place-hidden.js

--- js/place-hidden.js
+++ js/place-hidden.js
@@ -11,22 +11,6 @@ function closeMenu() {
   isOpen = false; // 메뉴가 닫히므로 상태 false로 설정
 }
 
-function closeInfo() {
-  placeInfo.style.display = "none";
-  isInfoVisible = false;
-
-  // 메뉴 목록 다시 보이게
-  placeButtons.classList.remove("hidden-by-info");
-}
-
-function openInfo() {
-  placeInfo.style.display = "none";
-  isInfoVisible = true;
-
-  // 메뉴는 숨기기
-  placeButtons.classList.add("hidden-by-info");
-}
-
 function openMenu() {
   placeButtons.classList.add("active");
   toggleBtn.innerHTML = "&gt;"; // 토글 버튼 방향
@@ -72,6 +56,34 @@ document.addEventListener("click", function (event) {
 });
 
 setupMapClickEvent();
+
+function openInfo() {
+  placeInfo.style.display = "block";
+  isInfoVisible = true;
+
+  // 메뉴는 숨기기
+  placeButtons.classList.add("hidden-by-info");
+}
+
+// 장소 정보창을 닫을 때 호출되는 함수
+function closeInfo() {
+  const placeInfo = document.getElementById("place-info");
+  placeInfo.style.display = "none"; // 창을 숨깁니다.
+  isInfoVisible = false; // 정보창이 닫혔으므로 상태를 false로 설정
+  // 메뉴 목록 다시 보이게
+  placeButtons.classList.remove("hidden-by-info");
+}
+
+// 메뉴 버튼 클릭 시 장소 목록을 표시하고 정보창 상태 확인
+function showPlaceButtons() {
+  if (!isInfoVisible) {
+    // 정보창이 보이지 않으면 장소 목록을 표시
+    placeButtons.classList.add("hidden-by-info");
+  } else {
+    // 정보창이 열려 있다면 장소 목록을 숨깁니다.
+    placeButtons.classList.remove("hidden-by-info");
+  }
+}
   
   // 지도 클릭 시 장소 목록을 숨기기
   // if (map) {
@@ -90,25 +102,6 @@ setupMapClickEvent();
   //   console.error("Map is not initialized yet.");
   // }
   // });
-
-// 장소 정보창을 닫을 때 호출되는 함수
-function closeInfo() {
-  const placeInfo = document.getElementById("place-info");
-  placeInfo.style.display = "none"; // 창을 숨깁니다.
-  isInfoVisible = false; // 정보창이 닫혔으므로 상태를 false로 설정
-  showPlaceButtons();
-}
-
-// 메뉴 버튼 클릭 시 장소 목록을 표시하고 정보창 상태 확인
-function showPlaceButtons() {
-  if (!isInfoVisible) {
-    // 정보창이 보이지 않으면 장소 목록을 표시
-    placeButtons.classList.add("hidden-by-info");
-  } else {
-    // 정보창이 열려 있다면 장소 목록을 숨깁니다.
-    placeButtons.classList.remove("hidden-by-info");
-  }
-}
 
 // function setupMapClickEvent() {
 //   if (!map) {
