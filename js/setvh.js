fc12f2c5bc99ec31575ac5c49735011fb191dba7
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-05-06 02:00:58 +0900

    Update setvh.js

--- js/setvh.js
+++ js/setvh.js
@@ -3,10 +3,18 @@ function setVhUnit() {
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
+function setVwUnit() {
+  const vw = window.innerWidth * 0.01; // 너비를 기준으로 1% 계산
+  document.documentElement.style.setProperty('--vw', `${vw}px`);
+}
+
 setVhUnit();
+setVwUnit();
 
 // DOM이 준비된 후 실행되도록!
 window.addEventListener('load', () => {
   window.addEventListener('resize', setVhUnit);
   window.addEventListener('orientationchange', setVhUnit);
+  window.addEventListener('resize', setVwUnit); // 화면 크기 변경 시 갱신
+  window.addEventListener('orientationchange', setVwUnit); // 방향 전환 시 갱신
 });
